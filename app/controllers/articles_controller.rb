5facfd2adb79a6a12eb3290f8549673376c999a4
sanjayyuvasoft378 <sanjayyuvasoft378@gmail.com>
2023-04-04 18:35:19 +0530

    from_scratch

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @articles = Article.find(params[:id])
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(title:"...", body:"...")
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status:unprocessable_entity
+    end
+  end
+end
